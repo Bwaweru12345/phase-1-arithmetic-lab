e06782bd997cf76f49b523a32f71cd6137a12685
Bwaweru12345 <bryanwaweru1076@example.com>
2022-12-07 12:49:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+// Write your code here
+
+let multiply = (num1 *= num2);
+ multiply;
+
+ let random = Math.floor(Math.random() * 10) + 1;
+
+
+ let num3 = 22
+ let num4 = 6
+ let mod = (num3 %= num4);
+ mod;
+
+ let max = Math.max(0,20)
